1c6e2685e2d463f5be7dc704791416edb5ecf050
Sophie Lee <sblee092@gmail.com>
2019-09-07 19:48:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
     array.shift();
     return array;
   }
+
+function removeElementFromBeginningOfArray(array){
+	  return array.slice(1);
+	}
