98fcd66dabab2c35099304c92db9892f9168dc6f
Sophie Lee <sblee092@gmail.com>
2019-09-07 19:31:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,11 +2,8 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 
 function addElementToBeginningOfArray(array, element) {
-	array.unshift(element);
-	return array;
-	//return [element,...array];
+	return [element,...array];
 }
-addElementToBeginningOfArray([1], 'foo')
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 	array.unshift(element);
